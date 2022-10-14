8fddb9c5873cb17109a6971ffcbcb5677548386e
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2022-10-14 00:55:29 -0400

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({origin:['http://127.0.0.1:5172','http://127.0.0.1:5173']});
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
