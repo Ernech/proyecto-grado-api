7777ae188a7b56710afa3904d83e7c993ab92c7d
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2022-10-14 23:24:33 -0400

    Credentials true

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin:['http://127.0.0.1:5172','http://127.0.0.1:5173']});
+  app.enableCors({origin:['http://127.0.0.1:5172','http://127.0.0.1:5173'],credentials: true,});
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
