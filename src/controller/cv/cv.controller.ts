1d67075030dcad29fdc37534560f6a9b8696d8f9
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2022-10-17 17:57:23 -0400

    Get cv

--- src/controller/cv/cv.controller.ts
+++ src/controller/cv/cv.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
 import { Roles } from 'src/decorator/role.decorator';
 import { CVInfoDTO } from 'src/dto/cv-info.dto';
 import { RoleType } from 'src/persistence/enum/role-type.enum';
@@ -17,6 +17,13 @@ export class CvController {
         return this.cvService.saveCV(candidateId,cvInfoDTO);
 
     }
-
+    
+    @Get('candidate/:id')
+    @Roles(RoleType.CANDIDATE)
+    async getCVByCandidateId(@Param('id',new ParseUUIDPipe()) candidateId:string){
+       return "Get cv " + candidateId
+        // return this.cvService.getCVByCandidateId(candidateId)
+    }
+    
 
 }
